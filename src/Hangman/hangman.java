bff39fe872c82d375dc2a07b620e5012e21790a8
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-03 19:31:02 +0200

    Hangman 5 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,16 +8,55 @@ public class hangman {
         String[] words = {"python", "java", "javascript", "kotlin"};
         Random random = new Random();
         String secretWord = words[random.nextInt(words.length)];
-        String hint = secretWord.substring(0, 2) + "-".repeat(secretWord.length() - 2);
-        System.out.print("Guess the word " + hint + ": > ");
-        Scanner scanner = new Scanner(System.in);
-        String guessedWord = scanner.nextLine();
 
-        if (guessedWord.equalsIgnoreCase(secretWord)) {
-            System.out.println("You survived!");
-        } else {
+        char[] guessedWord = new char[secretWord.length()];
+        for (int i = 0; i < guessedWord.length; i++) {
+            guessedWord[i] = '-';
+        }
+
+        HashSet<Character> usedLetters = new HashSet<>();
+        int attemptsLeft = 8;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (attemptsLeft > 0) {
+            System.out.println(guessedWord);
+            System.out.print("Input a letter: > ");
+            char guess = scanner.next().charAt(0);
+
+            if (usedLetters.contains(guess)) {
+                System.out.println("You already tried that letter.");
+                continue;
+            }
+
+            usedLetters.add(guess);
+
+            boolean found = false;
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.charAt(i) == guess) {
+                    guessedWord[i] = guess;
+                    found = true;
+                }
+            }
+
+            if (found) {
+                if (String.valueOf(guessedWord).equals(secretWord)) {
+                    System.out.println(guessedWord);
+                    System.out.println("You survived!");
+                    break;
+                }
+            } else {
+                System.out.println("That letter doesn't appear in the word.");
+                attemptsLeft--;
+            }
+        }
+
+        if (attemptsLeft == 0) {
             System.out.println("You lost!");
         }
+
+        System.out.println("Thanks for playing!");
+        System.out.println("We'll see how well you did in the next stage.");
 
         scanner.close();
     }

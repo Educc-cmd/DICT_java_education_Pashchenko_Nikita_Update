13c5d7ba1f2829263b2f66e9aafec13a7bfbc099
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-03 14:42:39 +0200

    ChatBot 1 stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String botName = "Chappa";
+        int birthYear = 2024;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}

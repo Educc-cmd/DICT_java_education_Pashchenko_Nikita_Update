df4d283ed334cf0b344523425d33aad465e3b691
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-03 19:15:21 +0200

    Hangman 1 stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,6 @@
+public class hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}

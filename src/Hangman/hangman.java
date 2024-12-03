640b4841773022fd4f995ecb799be227faeaf6a8
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-03 19:27:40 +0200

    Hangman 4 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -7,7 +7,8 @@ public class hangman {
         String[] words = {"python", "java", "javascript", "kotlin"};
         Random random = new Random();
         String secretWord = words[random.nextInt(words.length)];
-        System.out.print("Guess the word: > ");
+        String hint = secretWord.substring(0, 2) + "-".repeat(secretWord.length() - 2);
+        System.out.print("Guess the word " + hint + ": > ");
         Scanner scanner = new Scanner(System.in);
         String guessedWord = scanner.nextLine();
 

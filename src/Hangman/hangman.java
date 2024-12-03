4901d3b02c58182db1c7c24d89b1fc9b0e040960
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-03 19:21:39 +0200

    Hangman 2 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        String secretWord = "java";
+        System.out.print("Guess the word: > ");
+        Scanner scanner = new Scanner(System.in);
+        String guessedWord = scanner.nextLine();
+
+        if (guessedWord.equalsIgnoreCase(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
     }
 }

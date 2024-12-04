cf857bd87c65fde86b8c78d7e6374405167c6e61
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-04 15:24:55 +0200

    CoffeeMachine 4 stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,51 +1,112 @@
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    private static final int WATER_PER_CUP = 200; // мл води
-    private static final int MILK_PER_CUP = 50;  // мл молока
-    private static final int BEANS_PER_CUP = 15; // г кавових зерен
+    private static int water = 400;
+    private static int milk = 540;
+    private static int coffeeBeans = 120;
+    private static int disposableCups = 9;
+    private static int money = 550;
 
     public static void main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.checkIngredientsAndMakeCoffee();
+        coffeeMachine.run();
     }
 
-    public void checkIngredientsAndMakeCoffee() {
+    public void run() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введіть, скільки мл води є у кавомашині:");
-        int availableWater = scanner.nextInt();
+        while (true) {
+            System.out.println("Кавомашина має:");
+            System.out.println(water + " мл води");
+            System.out.println(milk + " мл молока");
+            System.out.println(coffeeBeans + " г кавових зерен");
+            System.out.println(disposableCups + " одноразових стаканчиків");
+            System.out.println(money + " грн");
 
-        System.out.println("Введіть, скільки мл молока є у кавомашині:");
-        int availableMilk = scanner.nextInt();
+            System.out.println("Виберіть дію (buy, fill, take):");
+            String action = scanner.nextLine();
 
-        System.out.println("Введіть, скільки грамів кавових зерен є у кавомашині:");
-        int availableBeans = scanner.nextInt();
-
-        System.out.println("Введіть, скільки чашок кави ви хочете приготувати:");
-        int requiredCups = scanner.nextInt();
-
-        int requiredWater = requiredCups * WATER_PER_CUP;
-        int requiredMilk = requiredCups * MILK_PER_CUP;
-        int requiredBeans = requiredCups * BEANS_PER_CUP;
-
-        int maxCupsByWater = availableWater / WATER_PER_CUP;
-        int maxCupsByMilk = availableMilk / MILK_PER_CUP;
-        int maxCupsByBeans = availableBeans / BEANS_PER_CUP;
-
-        int maxCups = Math.min(Math.min(maxCupsByWater, maxCupsByMilk), maxCupsByBeans);
-
-        if (requiredCups == 0) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + maxCups + " more than that)");
-        } else if (maxCups >= requiredCups) {
-            int extraCups = maxCups - requiredCups;
-            if (extraCups > 0) {
-                System.out.println("Yes, I can make that amount of coffee (and even " + extraCups + " more than that)");
-            } else {
-                System.out.println("Yes, I can make that amount of coffee");
+            if (action.equals("buy")) {
+                buyCoffee(scanner);
+            } else if (action.equals("fill")) {
+                fillMachine(scanner);
+            } else if (action.equals("take")) {
+                takeMoney();
             }
-        } else {
-            System.out.println("No, I can make only " + maxCups + " cups of coffee");
         }
+    }
+
+    public void buyCoffee(Scanner scanner) {
+        System.out.println("Що ви хочете купити? 1 - еспресо, 2 - лате, 3 - капучино:");
+        int coffeeChoice = Integer.parseInt(scanner.nextLine());
+
+        int requiredWater = 0;
+        int requiredMilk = 0;
+        int requiredBeans = 0;
+        int price = 0;
+
+        switch (coffeeChoice) {
+            case 1: // еспресо
+                requiredWater = 250;
+                requiredBeans = 16;
+                price = 4;
+                break;
+            case 2: // лате
+                requiredWater = 350;
+                requiredMilk = 75;
+                requiredBeans = 20;
+                price = 7;
+                break;
+            case 3: // капучино
+                requiredWater = 200;
+                requiredMilk = 100;
+                requiredBeans = 12;
+                price = 6;
+                break;
+        }
+
+        if (water >= requiredWater && milk >= requiredMilk && coffeeBeans >= requiredBeans && disposableCups > 0) {
+            water -= requiredWater;
+            milk -= requiredMilk;
+            coffeeBeans -= requiredBeans;
+            disposableCups--;
+            money += price;
+
+            System.out.println("Ваша кава готова! Ви купили каву за " + price + " грн.");
+        } else {
+            if (water < requiredWater) {
+                System.out.println("Перепрошую, недостатньо води!");
+            }
+            if (milk < requiredMilk) {
+                System.out.println("Перепрошую, недостатньо молока!");
+            }
+            if (coffeeBeans < requiredBeans) {
+                System.out.println("Перепрошую, недостатньо кавових зерен!");
+            }
+            if (disposableCups <= 0) {
+                System.out.println("Перепрошую, недостатньо одноразових стаканчиків!");
+            }
+        }
+    }
+
+    public void fillMachine(Scanner scanner) {
+        System.out.println("Скільки мл води ви хочете додати?");
+        water += Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Скільки мл молока ви хочете додати?");
+        milk += Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Скільки грамів кавових зерен ви хочете додати?");
+        coffeeBeans += Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Скільки одноразових стаканчиків ви хочете додати?");
+        disposableCups += Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Запаси оновлені.");
+    }
+
+    public void takeMoney() {
+        System.out.println("Я вручив вам " + money + " грн");
+        money = 0;
     }
 }

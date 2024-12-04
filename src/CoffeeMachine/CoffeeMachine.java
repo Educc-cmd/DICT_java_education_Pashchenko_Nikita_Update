e2a7bb92be9f3a9923a3245e70f252e3d9334184
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-04 14:38:15 +0200

    CoffeeMachine 2 stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
+    private static final int WATER_PER_CUP = 200; // мл води
+    private static final int MILK_PER_CUP = 50;  // мл молока
+    private static final int BEANS_PER_CUP = 15; // г кавових зерен
+
     public static void main(String[] args) {
-        System.out.println("Починаю готувати каву");
-        System.out.println("Змелюю кавові зерна");
-        System.out.println("Кип'ячу воду");
-        System.out.println("Змішую кип'ячену воду з подрібненою кавою");
-        System.out.println("Наливаю каву в чашку");
-        System.out.println("Додаю трохи молока в чашку");
-        System.out.println("Кава готова!");
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.calculateIngredients();
+    }
+
+    public void calculateIngredients() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть, скільки чашок кави ви хочете приготувати:");
+        int numberOfCups = scanner.nextInt();
+
+        int totalWater = numberOfCups * WATER_PER_CUP;
+        int totalMilk = numberOfCups * MILK_PER_CUP;
+        int totalBeans = numberOfCups * BEANS_PER_CUP;
+
+        System.out.println("Для " + numberOfCups + " чашок кави вам знадобиться:");
+        System.out.println(totalWater + " мл води");
+        System.out.println(totalMilk + " мл молока");
+        System.out.println(totalBeans + " г кавових зерен");
     }
 }

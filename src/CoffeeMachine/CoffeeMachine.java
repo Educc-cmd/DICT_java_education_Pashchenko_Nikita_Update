0fbc3c64c4771dd839b9787b48deb8dc78bb330c
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-04 14:15:30 +0200

    CoffeeMachine 1 stage

---
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -0,0 +1,11 @@
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        System.out.println("Починаю готувати каву");
+        System.out.println("Змелюю кавові зерна");
+        System.out.println("Кип'ячу воду");
+        System.out.println("Змішую кип'ячену воду з подрібненою кавою");
+        System.out.println("Наливаю каву в чашку");
+        System.out.println("Додаю трохи молока в чашку");
+        System.out.println("Кава готова!");
+    }
+}

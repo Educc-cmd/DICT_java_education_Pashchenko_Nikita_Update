524dd3e91fa5a84f292f0e925a16c1015ef9e838
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-06 14:22:05 +0200

    TicTacToe 4 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -4,7 +4,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введіть комірки: "); // Запит на введення
+        System.out.print("Введіть комірки: ");
         String input = scanner.nextLine();
 
         if (input.length() != 9 || !input.matches("[XO_]+")) {
@@ -19,8 +19,35 @@ public class TicTacToe {
 
         printBoard(board);
 
-        String gameState = analyzeBoard(board);
-        System.out.println(gameState);
+        while (true) {
+            System.out.print("Введіть координати: ");
+
+            String coordinates = scanner.nextLine();
+            String[] parts = coordinates.split(" ");
+
+            if (parts.length != 2 || !parts[0].matches("\\d") || !parts[1].matches("\\d")) {
+                System.out.println("Ви повинні вводити числа!");
+                continue;
+            }
+
+            int row = Integer.parseInt(parts[0]);
+            int col = Integer.parseInt(parts[1]);
+
+            if (row < 1 || row > 3 || col < 1 || col > 3) {
+                System.out.println("Координати повинні бути від 1 до 3!");
+                continue;
+            }
+
+            if (board[row - 1][col - 1] != '_') {
+                System.out.println("Ця клітинка зайнята! Оберіть іншу!");
+                continue;
+            }
+
+            board[row - 1][col - 1] = 'X';
+
+            printBoard(board);
+            break;
+        }
     }
 
     private static void printBoard(char[][] board) {
@@ -33,47 +60,5 @@ public class TicTacToe {
             System.out.println("|");
         }
         System.out.println("---------");
-    }
-
-    private static String analyzeBoard(char[][] board) {
-        int xCount = 0, oCount = 0;
-        boolean xWins = false, oWins = false;
-
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                if (board[i][0] == 'X') xWins = true;
-                if (board[i][0] == 'O') oWins = true;
-            }
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
-                if (board[0][i] == 'X') xWins = true;
-                if (board[0][i] == 'O') oWins = true;
-            }
-
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == 'X') xCount++;
-                if (board[i][j] == 'O') oCount++;
-            }
-        }
-
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            if (board[0][0] == 'X') xWins = true;
-            if (board[0][0] == 'O') oWins = true;
-        }
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-            if (board[0][2] == 'X') xWins = true;
-            if (board[0][2] == 'O') oWins = true;
-        }
-
-        if (Math.abs(xCount - oCount) > 1 || (xWins && oWins)) {
-            return "Неможливо";
-        } else if (xWins) {
-            return "X виграв";
-        } else if (oWins) {
-            return "O виграв";
-        } else if (xCount + oCount < 9) {
-            return "Гра не завершена";
-        } else {
-            return "Нічия";
-        }
     }
 }

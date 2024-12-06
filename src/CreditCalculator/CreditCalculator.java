d54808bae0615ecb51f5becbf7944b2aad1dd73b
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-06 14:33:55 +0200

    CreditCalculator 1 stage

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,9 @@
+public class CreditCalculator {
+    public static void main(String[] args) {
+        System.out.println("Основна сума кредиту: 1000");
+        System.out.println("Місяць 1: виплачено 250");
+        System.out.println("Місяць 2: виплачено 250");
+        System.out.println("Місяць 3: виплачено 500");
+        System.out.println("Кредит погашено!");
+    }
+}

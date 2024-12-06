984f21c4da98e3a240e731eab443c943d60eaf5a
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-06 14:26:01 +0200

    TicTacToe 5 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -3,25 +3,18 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введіть комірки: ");
-        String input = scanner.nextLine();
-
-        if (input.length() != 9 || !input.matches("[XO_]+")) {
-            System.out.println("Некоректний ввід. Використовуйте лише символи 'X', 'O' та '_'.");
-            return;
-        }
-
-        char[][] board = new char[3][3];
-        for (int i = 0; i < 9; i++) {
-            board[i / 3][i % 3] = input.charAt(i);
-        }
+        char[][] board = {
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'}
+        };
 
         printBoard(board);
 
+        char currentPlayer = 'X';
+
         while (true) {
             System.out.print("Введіть координати: ");
-
             String coordinates = scanner.nextLine();
             String[] parts = coordinates.split(" ");
 
@@ -43,10 +36,16 @@ public class TicTacToe {
                 continue;
             }
 
-            board[row - 1][col - 1] = 'X';
-
+            board[row - 1][col - 1] = currentPlayer;
             printBoard(board);
-            break;
+
+            String result = checkGameState(board);
+            if (!result.equals("Гра триває")) {
+                System.out.println(result);
+                break;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
         }
     }
 
@@ -60,5 +59,32 @@ public class TicTacToe {
             System.out.println("|");
         }
         System.out.println("---------");
+    }
+
+    private static String checkGameState(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != '_' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return board[i][0] + " виграє";
+            }
+            if (board[0][i] != '_' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return board[0][i] + " виграє";
+            }
+        }
+        if (board[0][0] != '_' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return board[0][0] + " виграє";
+        }
+        if (board[0][2] != '_' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return board[0][2] + " виграє";
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '_') {
+                    return "Гра триває";
+                }
+            }
+        }
+
+        return "Нічия";
     }
 }

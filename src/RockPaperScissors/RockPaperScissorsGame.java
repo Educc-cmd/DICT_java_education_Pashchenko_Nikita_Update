affdd04beff28e13d69be82ca0b35f6e98b95243
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-09 17:33:41 +0200

    RockPaperScissors 2 stage

--- src/RockPaperScissors/RockPaperScissorsGame.java
+++ src/RockPaperScissors/RockPaperScissorsGame.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
+import java.util.Random;
 
 interface Game {
     void play(String userChoice);
 }
 
 class RockPaperScissors implements Game {
+    private Random random = new Random();
+
     @Override
     public void play(String userChoice) {
-        String computerChoice = getComputerChoice(userChoice);
-        System.out.println("Вибачте, але комп'ютер вибрав " + computerChoice);
+        String[] options = {"rock", "paper", "scissors"};
+        String computerChoice = options[random.nextInt(options.length)];
+
+        if (userChoice.equals(computerChoice)) {
+            System.out.println("Нічия. Обидва вибрали " + computerChoice);
+        } else if (isUserWinner(userChoice, computerChoice)) {
+            System.out.println("Молодець! Комп'ютер вибрав " + computerChoice + " і програв");
+        } else {
+            System.out.println("Вибачте, але комп'ютер вибрав " + computerChoice);
+        }
     }
 
-    private String getComputerChoice(String userChoice) {
-        switch (userChoice) {
-            case "rock":
-                return "paper";
-            case "paper":
-                return "scissors";
-            case "scissors":
-                return "rock";
-            default:
-                return "невірний вибір";
-        }
+    private boolean isUserWinner(String userChoice, String computerChoice) {
+        return (userChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                (userChoice.equals("paper") && computerChoice.equals("rock")) ||
+                (userChoice.equals("scissors") && computerChoice.equals("paper"));
     }
 }
 

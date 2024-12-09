3fa94dc73cd440296169aee9109c5c90de3518a1
Educc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-12-09 23:03:42 +0200

    RockPaperScissors 5 stage

--- src/RockPaperScissors/RockPaperScissorsGame.java
+++ src/RockPaperScissors/RockPaperScissorsGame.java
@@ -1,79 +1,48 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
-interface Game {
-    void play(String userChoice);
-}
+class RockPaperScissorsGame {
+    private static Map<String, Integer> ratings = new HashMap<>();
+    private static int userScore = 0;
+    private static List<String> options = Arrays.asList("rock", "paper", "scissors");
+    private static final Random random = new Random();
 
-class RockPaperScissors implements Game {
-    private Random random = new Random();
-    private int score = 0;
-
-    private static final Map<String, String> WINNING_COMBINATIONS = new HashMap<>() {{
-        put("rock", "scissors");
-        put("paper", "rock");
-        put("scissors", "paper");
-    }};
-
-    @Override
-    public void play(String userChoice) {
-        String[] options = {"rock", "paper", "scissors"};
-        String computerChoice = options[random.nextInt(options.length)];
-
-        if (userChoice.equals(computerChoice)) {
-            System.out.println("Нічия. Обидва вибрали " + computerChoice);
-            score += 50;
-        } else if (WINNING_COMBINATIONS.get(userChoice).equals(computerChoice)) {
-            System.out.println("Молодець! Комп'ютер вибрав " + computerChoice + " і програв");
-            score += 100;
-        } else {
-            System.out.println("Вибачте, але комп'ютер вибрав " + computerChoice);
-        }
-    }
-
-    public int getScore() {
-        return score;
-    }
-}
-
-public class RockPaperScissorsGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Integer> ratings = readRatings();
 
-        // Введення імені користувача
         System.out.print("Введіть своє ім'я: ");
-        String name = scanner.nextLine();
-        System.out.println("Привіт, " + name);
+        String userName = scanner.nextLine();
+        System.out.println("Привіт, " + userName);
 
-        // Ініціалізація рахунку користувача
-        int userScore = ratings.getOrDefault(name, 0);
+        loadRatings();
+        userScore = ratings.getOrDefault(userName, 0);
 
-        // Початок гри
-        RockPaperScissors game = new RockPaperScissors();
+        System.out.print("Введіть опції гри (розділені комами) або залиште порожнім для стандартних: ");
+        String inputOptions = scanner.nextLine();
+        if (!inputOptions.isEmpty()) {
+            options = Arrays.asList(inputOptions.split(","));
+        }
+        System.out.println("Добре, розпочнемо!");
+
         while (true) {
-            System.out.print("Введіть ваш вибір (rock, paper, scissors), !rating або !exit: ");
+            System.out.print("Введіть ваш вибір або !exit чи !rating: ");
             String userInput = scanner.nextLine();
 
             if (userInput.equals("!exit")) {
-                System.out.println("Bye!");
+                System.out.println("До побачення!");
                 break;
             } else if (userInput.equals("!rating")) {
-                System.out.println("Ваш рахунок: " + (userScore + game.getScore()));
-            } else if (userInput.equals("rock") || userInput.equals("paper") || userInput.equals("scissors")) {
-                game.play(userInput);
+                System.out.println("Ваш рахунок: " + userScore);
+            } else if (options.contains(userInput)) {
+                playGame(userInput);
             } else {
-                System.out.println("Invalid input");
+                System.out.println("Невірне введення");
             }
         }
     }
 
-    private static Map<String, Integer> readRatings() {
-        Map<String, Integer> ratings = new HashMap<>();
+    private static void loadRatings() {
         try (Scanner fileScanner = new Scanner(new File("rating.txt"))) {
             while (fileScanner.hasNext()) {
                 String name = fileScanner.next();
@@ -81,8 +50,29 @@ public class RockPaperScissorsGame {
                 ratings.put(name, score);
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Файл rating.txt не знайдено, створюємо новий рахунок.");
+            System.out.println("Файл рейтингів не знайдено. Починаємо з нуля.");
         }
-        return ratings;
+    }
+
+    private static void playGame(String userChoice) {
+        String computerChoice = options.get(random.nextInt(options.size()));
+
+        if (userChoice.equals(computerChoice)) {
+            System.out.println("Нічия (" + computerChoice + ")");
+            userScore += 50;
+        } else if (isUserWin(userChoice, computerChoice)) {
+            System.out.println("Молодець! Комп'ютер вибрав " + computerChoice + " і програв");
+            userScore += 100;
+        } else {
+            System.out.println("Вибачте, але комп'ютер вибрав " + computerChoice);
+        }
+    }
+
+    private static boolean isUserWin(String userChoice, String computerChoice) {
+        int userIndex = options.indexOf(userChoice);
+        int computerIndex = options.indexOf(computerChoice);
+        int halfSize = options.size() / 2;
+
+        return (userIndex - computerIndex + options.size()) % options.size() <= halfSize;
     }
 }
